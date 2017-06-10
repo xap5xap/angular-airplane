1a1a4c7eb6b714e7bd51b12300704bc2da21b187
Xavier Perez <xap5xap@gmail.com>
2017-06-10 17:33:36 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Game from '../game/Game';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  ngOnInit() {
+    console.log('hola');
+    const game: Game = new Game();
+    game.init();
+  }
 }
